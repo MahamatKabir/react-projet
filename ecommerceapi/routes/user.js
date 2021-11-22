4dee2fb79f5315f191e8d052093a8fefdb8e725a
MahamatKabir <mahamatkabir1@gmail.com>
2021-11-22 17:49:29 +0300

    mongodb

---
+++ ecommerceapi/routes/user.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+
+ /*router.get('/usertest',  (req, res) => {
+    res.send("my databde is successful");
+});
+
+ router.post('/userposttest', (req, res) => {
+    const username = req.body.username;
+    res.send("your user name is " + username);
+})*/
+
+module.exports = router;
