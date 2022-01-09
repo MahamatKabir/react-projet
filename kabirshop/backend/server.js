516c7d61e93c4def7f4c45f4dfb2d8e09220bc1d
MahamatKabir <mahamatkabir1@gmail.com>
2022-01-10 00:51:46 +0300

    keroku

--- kabirshop/backend/server.js
+++ kabirshop/backend/server.js
@@ -38,11 +38,25 @@ const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
 
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+  app.get('*', (req, res)=> 
+    res.sendFile(path.resolve(__dirname, 'frontend','build', 'index.html')) 
+  )
+}else{
+  app.get('/', (req, res)=> {
+   res.send('API is running.....')
+  })
+}
+
+
+
 
 app.use(notFound)
  
 app.use(errorHandler)
-const PORT = process.env.PORT || 8000;      
+const PORT = process.env.PORT || 8000;       
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
 
